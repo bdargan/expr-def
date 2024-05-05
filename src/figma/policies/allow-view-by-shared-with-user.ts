723977cb529deeb37d53a14e476d33ecd0f606cf
Brett Dargan <brett.dargan@gmail.com>
2024-05-05 12:51:59 +1000

    wip: allow explicit share with new array ops

---
+++ src/figma/policies/allow-view-by-shared-with-user.ts
@@ -0,0 +1,16 @@
+import { ExpressionDef, Effect, ResourceType, FilePermission, Permission, Policy } from '../model'
+
+export class AllowViewByShareWithUser implements Policy {
+  readonly description: string
+  readonly permissions: Permission[]
+  readonly resourceType: ResourceType
+  readonly applyFilter: ExpressionDef
+  readonly effect: Effect
+  constructor() {
+    this.description = 'Allow view of file by explicit sharing.'
+    this.resourceType = 'file'
+    this.effect = 'allow'
+    this.permissions = [FilePermission.can_view]
+    this.applyFilter = ['user', '=', 'file.shared_viewers']
+  }
+}
